12b8ae364dccfb264d616b89f121198d18a47779
MyPc <brianw119@outlook.com>
2024-06-13 00:17:24 +0100

    input form added

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 const taskReminder = {
    grid1: document.querySelector(".grid1"),
    grid2: document.querySelector(".grid2"),
-   title: document.querySelector(".title"),
-   details: document.querySelector(".details"),
-   dueDate: document.querySelector(".dueDate"),
-   newTask: document.querySelector(".taskNew"),
+   title: null,
+   details: null,
+   dueDate: null,
+   priority: null,
+   newTask: document.querySelector("#taskNew"),
+   container: document.querySelector("#container"),
+   enter: document.querySelector("#enter"),
+   clear: document.querySelector("#clear"),
 
    //creates new element
    newElement(type) {
@@ -12,14 +16,27 @@ const taskReminder = {
       return newItem;
    },
 
-   createForm() {},
+   createForm() {
+      this.title = this.newElement("input");
+      this.title.setAttribute("id", "title");
+      this.title.style.backgroundColor = "brown";
+      this.container.appendChild(this.title);
+   },
 
    //new task class
    taskNew: {
-      constructor(title, dueDate, priority) {
+      constructor(title, details, dueDate, priority) {
          this.title = title;
+         this.details = details;
          this.dueDate = dueDate;
          this.priority = priority;
       },
    },
+
+   init() {
+      this.newTask.addEventListener("click", () => {
+         this.createForm();
+      });
+   },
 };
+taskReminder.init();
