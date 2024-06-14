ce3d8bee666bc0839f0afe7655d7ba7d88ca6302
MyPc <brianw119@outlook.com>
2024-06-14 23:49:43 +0100

    Styling priority fields

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 const taskReminder = {
    grid1: document.querySelector(".grid1"),
    grid2: document.querySelector(".grid2"),
-   title: null,
-   details: null,
-   dueDate: null,
-   priority: null,
+   title: document.querySelector("#title"),
+   details: document.querySelector("#details"),
+   dueDate: document.querySelector("#date"),
+   form: document.querySelector("#form"),
+   priority: document.querySelector("#details"),
    newTask: document.querySelector("#taskNew"),
    container: document.querySelector("#container"),
    enter: document.querySelector("#enter"),
    clear: document.querySelector("#clear"),
    enterClearBox: document.querySelector(".buttonsLower"),
+   pendingTasks: document.querySelector(".pendingTasks"),
 
    //creates new element
    newElement(type) {
@@ -17,17 +19,10 @@ const taskReminder = {
       return newItem;
    },
 
-   createForm() {
-      this.title = this.newElement("input");
-      this.title.setAttribute("id", "title");
-      this.title.style.backgroundColor = "brown";
-      this.container.appendChild(this.title);
-   },
-
    //new task class
    taskNew: {
       constructor(title, details, dueDate, priority) {
-         this.title = title;
+         this.title = "title";
          this.details = details;
          this.dueDate = dueDate;
          this.priority = priority;
@@ -40,14 +35,41 @@ const taskReminder = {
       for (let i = 0; i < buttons.length; i++) {
          this.grid1.removeChild(buttons[i]);
       }
+      this.grid2.removeChild(this.pendingTasks);
    },
 
-   //adds user input field to the DOM
+   //adds user input fields to the DOM
    addInterface() {
       const buttons = [this.container, this.enterClearBox];
       for (let i = 0; i < buttons.length; i++) {
          this.grid1.appendChild(buttons[i]);
       }
+      this.grid2.appendChild(this.pendingTasks);
+   },
+
+   //captures data from input field
+   captureInputData() {
+      const arr = [];
+      this.enter.addEventListener("click", () => {
+         const input = [
+            this.title.value,
+            this.details.value,
+            this.dueDate.value,
+            this.priority.value,
+         ];
+
+         for (let i = 0; i < input.length; i++) {
+            arr.push(input[i]);
+         }
+         console.log(arr);
+      });
+   },
+
+   //resets form to accept new input
+   clearInputFields() {
+      this.clear.addEventListener("click", () => {
+         this.form.reset();
+      });
    },
 
    init() {
@@ -57,6 +79,8 @@ const taskReminder = {
       this.newTask.addEventListener("click", () => {
          this.addInterface();
       });
+      this.captureInputData();
+      this.clearInputFields();
    },
 };
 taskReminder.init();
