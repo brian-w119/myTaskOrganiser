0fecb0bf5cc5e154e51012cec5f5eafc4435a186
MyPc <brianw119@outlook.com>
2024-06-25 20:19:53 +0100

    Removed class that contains input details

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ const taskReminder = {
    //new task class
    taskNew: {
       constructor(title, details, dueDate, priority) {
-         this.title = "title";
+         this.title = title;
          this.details = details;
          this.dueDate = dueDate;
          this.priority = priority;
